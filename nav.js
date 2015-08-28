fbbaf0224562a4a3a21f47ee90312eb073c8734f
Levi Durfee <levi@teklife.info>
2015-08-28 09:41:03 -0400

    Added sample code

---
+++ nav.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$('.navigation > li > a').each( function(){
+		var $this = $(this);
+		$newLI = $('<li>').append( $this.clone() );
+		$newLI.insertBefore($this.next('ul').find('.first'));
+	});
+    
+    $("ul li ul").hide();
+    
+    $(".level-1").click(function(event) {
+        $("ul li ul").hide();
+        $(this).children("li ul").toggle();
+        event.preventDefault();
+    });
+});
